52a76dc17cacf4e8d8652a520822e85cfb0ba8c9
Tatiana-Menjivar17 <tatiavane1705@gmail.com>
2021-09-14 20:19:35 -0600

    Se arregla el register.js

--- farmastuff/app/controllers/private/register.js
+++ farmastuff/app/controllers/private/register.js
@@ -1,26 +1,22 @@
 // Constante para establecer la ruta y parámetros de comunicación con la API.
-const API_USUARIOS = '../app/api/usuarios.php?action=';
+const API_EMPLEADOS = '../../app/api/private/empleados.php?action=';
+
+var inputClave = document.getElementById('clave');
+var inputConfClave = document.getElementById('confclave');
+var inputNombre = document.getElementById('nombreempleado');
+var inputUsuario = document.getElementById('usuario');
+var inputTelefono = document.getElementById('telefonoempleado');
+var inputCorreo = document.getElementById('correoempleado');
+var inputDireccion = document.getElementById('direccionempleado');
+var inputApellido = document.getElementById('apellidoempleado');
 
 // Método manejador de eventos que se ejecuta cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', function () {
     // Se inicializa el componente Tooltip asignado al botón del formulario para que funcione la sugerencia textual.
     M.Tooltip.init(document.querySelectorAll('.tooltipped'));
 
-    //PARA FECHA
-    let today = new Date();
-        // Se declara e inicializa una variable para guardar el día en formato de 2 dígitos.
-        let day = ('0' + today.getDate()).slice(-2);
-        // Se declara e inicializa una variable para guardar el mes en formato de 2 dígitos.
-        var month = ('0' + (today.getMonth() + 1)).slice(-2);
-        // Se declara e inicializa una variable para guardar el año con la mayoría de edad.
-        let year = today.getFullYear() - 18;
-        // Se declara e inicializa una variable para establecer el formato de la fecha.
-        let date = `${year}-${month}-${day}`;
-        // Se asigna la fecha como valor máximo en el campo del formulario.
-        document.getElementById('fecha_nacimiento').setAttribute('max', date);
-
-        // Petición para verificar si existen usuarios.
-    fetch(API_USUARIOS + 'readAll', {
+    // Petición para verificar si existen usuarios.
+    fetch(API_EMPLEADOS + 'readAll', {
         method: 'get'
     }).then(function (request) {
         // Se verifica si la petición es correcta, de lo contrario se muestra un mensaje indicando el problema.
@@ -28,13 +24,31 @@ document.addEventListener('DOMContentLoaded', function () {
             request.json().then(function (response) {
                 // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje.
                 if (response.status) {
-                    sweetAlertM(3, response.message, 'index.php');
+                    sweetAlert(3, response.message, 'login.php');
+                    inputDireccion.disabled = true;
+                    inputCorreo.disabled = true;
+                    inputTelefono.disabled = true;
+                    inputApellido.disabled = true;
+                    inputUsuario.disabled = true;
+                    inputNombre.disabled = true;
+                    inputClave.disabled = true;
+                    inputConfClave.disabled = true;
+                    
                 } else {
                     // Se verifica si ocurrió un problema en la base de datos, de lo contrario se continua normalmente.
                     if (response.error) {
-                        sweetAlertM(2, response.exception, null);
+                        sweetAlert(2, response.exception, null);
+                        inputDireccion.disabled = true;
+                        inputCorreo.disabled = true;
+                        inputTelefono.disabled = true;
+                        inputApellido.disabled = true;
+                        inputUsuario.disabled = true;
+                        inputNombre.disabled = true;
+                        inputClave.disabled = true;
+                        inputConfClave.disabled = true;
+
                     } else {
-                        sweetAlertM(4, 'Debe crear un usuario para comenzar', null);
+                        sweetAlert(4, 'Debe crear un usuario para comenzar', null);
                     }
                 }
             });
@@ -51,7 +65,7 @@ document.getElementById('register-form').addEventListener('submit', function (ev
     // Se evita recargar la página web después de enviar el formulario.
     event.preventDefault();
 
-    fetch(API_USUARIOS + 'register', {
+    fetch(API_EMPLEADOS + 'register', {
         method: 'post',
         body: new FormData(document.getElementById('register-form'))
     }).then(function (request) {
@@ -60,9 +74,10 @@ document.getElementById('register-form').addEventListener('submit', function (ev
             request.json().then(function (response) {
                 // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
                 if (response.status) {
-                    sweetAlertM(1, response.message, 'index.php');
+                    sweetAlert(1, response.message, 'login.php');
+
                 } else {
-                    sweetAlertM(2, response.exception, null);
+                    sweetAlert(2, response.exception, 'Estamos probando');
                 }
             });
         } else {
