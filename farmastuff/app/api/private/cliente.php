96af2fc03c5cdf2cd01d2ec46265ff17d009e61e
Tatiana Menjívar♥ <65384243+Tatiana-Menjivar17@users.noreply.github.com>
2021-09-17 01:14:25 -0600

    Update cliente.php

--- farmastuff/app/api/private/cliente.php
+++ farmastuff/app/api/private/cliente.php
@@ -11,7 +11,7 @@ require_once('../../models/clientes.php');
         // Se declara e inicializa un arreglo para guardar el resultado que retorna la API.
         $result = array('status' => 0, 'message' => null, 'exception' => null);
         // Se verifica si existe una sesión iniciada como administrador, de lo contrario se finaliza el script con un mensaje de error.
-    if (isset($_SESSION['idcliente'])) {
+    if (isset($_SESSION['idempleado'])) {
             // Se compara la acción a realizar cuando un administrador ha iniciado sesión.
             switch ($_GET['action']) {
                 case 'readAll':
