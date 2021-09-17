de3c59ab8382f21dde5745e297c63c3da048b3e1
Tatiana Menjívar♥ <65384243+Tatiana-Menjivar17@users.noreply.github.com>
2021-09-17 01:20:36 -0600

    Update empleados.php

--- farmastuff/app/api/private/empleados.php
+++ farmastuff/app/api/private/empleados.php
@@ -7,6 +7,7 @@ if (isset($_GET['action'])) {
     session_start();
     $empleados = new Empleados;
     $result = array('status' => 0, 'message' => null, 'exception' => null);
+    if (isset($_SESSION['idempleado'])) {
         switch ($_GET['action']) {
             case 'readAll':
                 if ($result['dataset'] = $empleados->readAll()) {
@@ -308,11 +309,17 @@ if (isset($_GET['action'])) {
                     $result['exception'] = 'Nombres incorrectos';
                 }
                 break;
+                
+            default:
+                $result['exception'] = 'Acción no disponible dentro de la sesión';
         }
         // Se indica el tipo de contenido a mostrar y su respectivo conjunto de caracteres.
         header('content-type: application/json; charset=utf-8');
         // Se imprime el resultado en formato JSON y se retorna al controlador.
         print(json_encode($result));
-} else {   
+    } else {
+        print(json_encode('Acceso denegado'));
+    }
+} else {
     print(json_encode('Recurso no disponible'));
 }
