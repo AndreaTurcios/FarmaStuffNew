bc3810d2e9a56b9b004c466e2095d92a5e94d4ab
Tatiana Menjívar♥ <65384243+Tatiana-Menjivar17@users.noreply.github.com>
2021-09-17 01:13:49 -0600

    Update cliente.php

--- farmastuff/app/api/private/cliente.php
+++ farmastuff/app/api/private/cliente.php
@@ -11,7 +11,7 @@ require_once('../../models/clientes.php');
         // Se declara e inicializa un arreglo para guardar el resultado que retorna la API.
         $result = array('status' => 0, 'message' => null, 'exception' => null);
         // Se verifica si existe una sesión iniciada como administrador, de lo contrario se finaliza el script con un mensaje de error.
-    //if (isset($_SESSION['idcliente'])) {
+    if (isset($_SESSION['idcliente'])) {
             // Se compara la acción a realizar cuando un administrador ha iniciado sesión.
             switch ($_GET['action']) {
                 case 'readAll':
@@ -215,9 +215,9 @@ require_once('../../models/clientes.php');
             header('content-type: application/json; charset=utf-8');
             // Se imprime el resultado en formato JSON y se retorna al controlador.
             print(json_encode($result));
-        // } else {
-        //     print(json_encode('Acceso denegado'));
-        // }
+        } else {
+             print(json_encode('Acceso denegado'));
+        }
     } else {
         print(json_encode('Recurso no disponible'));
     }
